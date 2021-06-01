6d33fce931fe533aa3bf0ccbb9141ca11c163ff1
shiyuq <1543082564@qq.com>
2021-06-01 18:43:56 +0800

    🎨 文档完善

--- cross-origin/demo/index.js
+++ cross-origin/demo/index.js
@@ -1,25 +1,41 @@
 const express = require('express');
+let app = express();
+let port;
 
 // 5001端口服务 将当前目录作为一个http服务
-let port = 5001;
-let app = express();
+port = 5001;
 app.use(express.static(__dirname));
 app.listen(port);
 
 // 5002端口服务 返回接口数据
 port = 5002;
-app = express();
 // 方案一：CORS 修改响应头
 app.get('/cors', function (req, res) {
   // 设置允许的源
-  res.header('Access-Control-Allow-Origin', '*');
-  res.send('hello world!');
+  whiteList = [ 'http://localhost:5001' ];
+  if (req.headers.origin && whiteList.indexOf(req.headers.origin) >= 0) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+  }
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  res.json({name: 'hello world!'});
 });
 
 // 方案二：JSONP
 app.get('/jsonp', function (req, res) {
-  const cb = req.query.callback;
-  res.send(`${cb}('hello world!')`);
+  // const cb = req.query.callback;
+  // res.send(`${cb}('hello world!')`);
+  app.set('jsonp callback name', 'callback'); // 回调名称
+  res.jsonp({ name: 'hello world!' });
+
+  // const jsonpCallback = app.get('jsonp callback name');
+  // const jsonp = req.query[jsonpCallback];
+  // if (jsonp) {
+  //   res.jsonp({ name: 'hello world!' });
+  // } else {
+  //   res.status(404).send();
+  // }
 });
 
 // 方案三：NGINX
